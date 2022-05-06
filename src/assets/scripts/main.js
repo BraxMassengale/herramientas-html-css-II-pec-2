4d26c508a338c983b7351dc62852e20a92aa5e83
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-05-06 22:43:11 +0200

    final commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,14 +3,15 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+var dropdown = new bootstrap.Dropdown('[data-bs-toggle="dropdown"]');
